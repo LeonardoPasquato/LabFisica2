7946f7011f7874e74331414078b69ccea81af086
Leonardo Pasquato <leonardo.pasquato@gmail.com>
2022-11-30 22:08:38 +0100

    grafico ampiezza

--- Lab3/PlotGrafici/bodePlot.py
+++ Lab3/PlotGrafici/bodePlot.py
@@ -1,18 +1,46 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import control
 import math
-
 import warnings
 warnings.filterwarnings('ignore')
 
-#G = control.tf([0.32*.00001,0],[0.1024*0.0000000005,0.32*0.00001,1])
-G = control.tf([0.00001, 0], [0.0000000005, 0.00001, 1])
+R = math.pow(10,4) 
+C = math.pow(10,-8)
+L = 500*math.pow(10,-3)
+
+a = R*C
+b = L*C
+c = R*C
+d = 1
+#transfer function {jw = s}
+G = control.tf([a, 0], [b, c, d])
 print(G)
 
-#w = np.logspace(-math.pi/2,math.pi/2)
-mag,phase,omega = control.bode(G,Hz=False,dB=True,deg=False)
+#points magnitude
+x = 2*math.pi
+ax = [100*x, 1000*x, 2000*x, 2500*x, 3000*x, 5000*x, 10000*x, 50000*x]
+ay = [0.312, 3, 4.68, 4.78, 4.5, 3.12, 1.62, 0.093]
+dby = [1,1,1,1,1,1,1,1]
+for i in range(8):
+    dby[i] = dby[i]*20 * math.log(ay[i]/5)
+print(dby)
 
+#plot magnitude and phase
+mag,phase,omega = control.bode(G,Hz=False,dB=True,deg=False,grid=True)
+
+fx = [100*x, 1000*x, 2000*x, 2500*x, 3000*x, 5000*x, 10000*x, 50000*x]
+fy = [2.68, 0.364, 0.017, 0.011, 0.02, 0.068, 0.0192, 0.0058]
+
+ax1 = plt.subplot(211)
+#plt.plot(omega,mag)
+plt.scatter(ax,dby, color = 'red')
+plt.tick_params('x', labelsize=6)
+
+ax2 = plt.subplot(212, sharex=ax1)
+plt.plot(omega,phase)
+plt.scatter(fx,fy)
+
+plt.xlabel('Frequenza angolare')
+plt.xscale('log')
 plt.show()
-
